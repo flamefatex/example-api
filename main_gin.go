a933e658bbd9ff9ed27a8fb9b962adb081186bb8
ouwenxian <ouwenxian@chinatelecom.cn>
2021-02-02 11:39:20 +0800

    新增环境变量处理

--- main_gin.go
+++ main_gin.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/flamefatex/config"
 	v1 "github.com/flamefatex/example-api/handler/v1"
 	"github.com/flamefatex/log"
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,11 @@ import (
 
 // gin
 func runGinServer() {
+	envGinAddr := config.Config().GetString("gin.addr")
+	if envGinAddr != "" {
+		ginAddr = envGinAddr
+	}
+
 	// handler 初始化
 	engine := gin.Default()
 	v1Group := engine.Group("/v1")
